b7cacdba20a103bb46d10c633d7987a1916d0d65
Zid95 <zid.abdulkarim@gmail.com>
2023-01-21 15:53:53 +0100

    design all product models

--- product/models.py
+++ product/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
@@ -29,13 +31,28 @@ class Product(models.Model):
 
 
 class ProductImages(models.Model):
-    pass
+    product = models.ForeignKey(Product,verbose_name=_('product'),related_name='product_image',on_delete=models.CASCADE)
+    image = models.ImageField(_('image'),upload_to='product_images/')
+
+    def __str__(self) -> str:
+        return str(self.product)
+
 
 class Brand(models.Model):
-    pass
+    name = models.CharField(_('brand'),max_length=50)
+    image = models.ImageField(_('image'),upload_to='brand/')
+
+
+    def __str__(self):
+        return self.name
 
 
 class Reviews(models.Model):
-    pass
-
-
+    user = models.ForeignKey(User,verbose_name=_('user'),related_name='review_author',on_delete=models.SET_NULL ,nul=True,blank=True)
+    product = models.ForeignKey(Product,verbose_name=_('product'),related_name='product_review',on_delete=models.CASCADE)
+    comment = models.CharField(_('comment'),max_length=200)
+    rate = models.IntegerField(_('rate'))
+    created_at = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return str(self.product)
