a53600d32e5e3466c3c90da7f80b7e7d7528be32
Zid95 <zid.abdulkarim@gmail.com>
2023-01-21 15:27:04 +0100

    design product model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from taggit.managers import TaggableManager
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+PRODUCT_FLAG = (
+    ('Sale','Sales'),
+    ('Feature','Feature'),
+    ('New','New'),
+)
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    image = models.ImageField(upload_to='products',default='default.png')
+    flag = models.CharField(choices=PRODUCT_FLAG,max_length=10)
+    price = models.FloatField()
+    sku = models.IntegerField()
+    brand = models.ForeignKey('Brand',related_name='product_brand',on_delete=models.CASCADE)
+    tags = TaggableManager()
+    subtitle = models.TextField(max_length=500)
+    description = models.TextField(max_length=20000)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+
+
+class ProductImages(models.Model):
+    pass
+
+class Brand(models.Model):
+    pass
+
+
+class Reviews(models.Model):
+    pass
+
+
