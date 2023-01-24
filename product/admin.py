3f43aa2936467ff74fd3a65ec24e199a97107f03
Zid95 <zid.abdulkarim@gmail.com>
2023-01-24 23:03:32 +0100

    new branch

--- product/admin.py
+++ product/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
+class ProductImagesAdmin(admin.TabularInline):
+    model = ProductImages
 
-admin.site.register(Product)
-admin.site.register(ProductImages)
+class ProductAdmin(admin.ModelAdmin):
+    list_display= ['name','brand','price']
+    list_filter = ['brand','price']
+    inlines = [ProductImagesAdmin]
+    search_fields = ['name','subtitle','description']
+
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['user','product','rate','comment','created_at']
+    list_filter = ['rate','created_at']
+    search_fields = ['user','product','comment']
+
+
+admin.site.register(Product,ProductAdmin)
 admin.site.register(Brand)
-admin.site.register(Reviews)
+admin.site.register(Reviews,ReviewAdmin)
