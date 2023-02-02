bde061c93a19754d56b70df66c36d49ed3c79122
Zid95 <zid.abdulkarim@gmail.com>
2023-02-02 12:35:28 +0100

    add admin customization on oders , products

--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Order)
-admin.site.register(OrderDetail)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['order_code','order_status','order_date','delivery_date']
+    list_filter = ['order_status','order_date']
+
+class OrderDetailAdmin(admin.ModelAdmin):
+    list_display = ['order','product','price','quantity','total']
+    list_filter = ['price','quantity']
+
+admin.site.register(Order,OrderAdmin)
+admin.site.register(OrderDetail,OrderDetailAdmin)
 
 admin.site.register(Cart)
 admin.site.register(CartDetail)
--- product/admin.py
+++ product/admin.py
@@ -5,10 +5,11 @@ class ProductImagesAdmin(admin.TabularInline):
     model = ProductImages
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display= ['name','brand','price']
+    list_display= ['id','name','brand','price']
     list_filter = ['brand','price']
     inlines = [ProductImagesAdmin]
     search_fields = ['name','subtitle','description']
+    list_editable = ['name','brand','price']
 
 
 
