faf3a25eb8d328bf3cff8f3dd01d0d0841677ab2
Zid95 <zid.abdulkarim@gmail.com>
2023-02-09 08:41:57 +0100

    add nav , footer with context processor

--- settings/models.py
+++ settings/models.py
@@ -15,3 +15,6 @@ class Company(models.Model):
     email = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
