b30a8a1bb32b18de9e9b604ff000ea6bf08a1378
Zid95 <zid.abdulkarim@gmail.com>
2023-02-12 00:06:28 +0100

    customize pagination

--- product/views.py
+++ product/views.py
@@ -31,9 +31,27 @@ class BrandList(ListView):
 
    
     
-class BrandDetail(DetailView):
-    model = Brand
+# class BrandDetail(DetailView):
+#     model = Brand
+    
  
+#     def get_queryset(self):
+#         queryset = Brand.objects.filter(slug=self.kwargs['slug']).annotate(product_count=Count('product_brand'))
+#         return queryset
+
+
+class BrandDetail(ListView):
+    model = Product
+    template_name = 'product/brand_detail.html'
+    context_object_name = 'products'
+    paginate_by = 25
+    
     def get_queryset(self):
-        queryset = Brand.objects.filter(slug=self.kwargs['slug']).annotate(product_count=Count('product_brand'))
+        brand = Brand.objects.get(slug=self.kwargs['slug'])
+        queryset = Product.objects.filter(brand=brand)
         return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["brand"] = (Brand.objects.filter(slug=self.kwargs['slug']).annotate(product_count=Count('product_brand')))[0]
+        return context
