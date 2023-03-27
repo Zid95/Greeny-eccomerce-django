a2dba25a06203949ba4ba4b14aa14043c70b0bb2
Zid95 <zid.abdulkarim@gmail.com>
2023-03-27 23:46:26 +0200

    add accounts_base page , accounts urls

--- project/urls.py
+++ project/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('product/', include('product.urls',namespace='product')),
     path('orders/',include('orders.urls',namespace='orders')),
